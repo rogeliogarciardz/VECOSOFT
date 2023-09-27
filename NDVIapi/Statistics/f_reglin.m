6b3ca55408107eb70e2938123f316939bd81dc61
rogeliogarciardz <rogeliogarciardz@gmail.com>
2023-09-27 12:12:07 -0500

    Reg lin

---
+++ NDVIapi/Statistics/f_reglin.m
@@ -0,0 +1,17 @@
+function [a0,a1,yajuste] = f_reglin(x,y)
+%fitlm
+    if(length(x)==length(y))
+        n = length(x);
+
+        a1 = ( (n * sum(x.*y,"omitnan")) - (sum(x,"omitnan")*sum(y,"omitnan") ))/ ( n * sum(x.^2,"omitnan") - sum(x,"omitnan")^2);
+        a0 = mean(y,'omitnan') - a1 * mean(x,'omitnan');
+        yajuste = a0+a1*x;
+
+    else
+        error("Arreglos de diferente tamaño") 
+    end
+    
+
+end
+
+
---
+++ NDVIapi/Statistics/f_reglin_3dpp.m
@@ -0,0 +1,16 @@
+function [ma0,ma1,yaj] = f_reglin_3dpp(M)
+tam_arr = size(M);
+M = permute(M,[1 3 2]);
+
+ma0 = zeros( tam_arr(1),tam_arr(2));
+ma1 = zeros(tam_arr(1),tam_arr(2));
+% yaj = zeros(tam_arr(3));
+
+tmp_x = 1:tam_arr(3);
+
+ for i=1:tam_arr(1)
+     for j=1:tam_arr(2)
+         [ma0(i,j),ma1(i,j),yaj] = m_reglin(tmp_x,M(i,:,j));
+     end
+ end
+end
