29e208d69f399a2226815f36580a34974050768a
rogeliogarciardz <rogeliogarciardz@gmail.com>
2023-11-22 16:41:23 -0600

    Agregar get_study area

--- NDVIapi/Preprocessing/f_rm_pixel_baja_calidad.m
+++ NDVIapi/Preprocessing/f_rm_pixel_baja_calidad.m
@@ -1,6 +1,7 @@
-function [data_n] = f_rm_pixel_baja_calidad(data,qa)
+function [data_n,nh] = f_rm_pixel_baja_calidad(data,qa)
    
         data_n=data;
         % eliminar datos de mala calidad
         data_n(qa ~= 0 & qa ~= 1)=nan;
+        nh= numel(data_n(isnan(data_n)));
 end
