be09e3fbd3ed794a385f190d91b604a511f6d939
rogeliogarciardz <rogeliogarciardz@gmail.com>
2024-01-29 15:48:19 -0600

    Fix bugs

--- NDVIapi/Statistics/f_reglin_3dpp.m
+++ NDVIapi/Statistics/f_reglin_3dpp.m
@@ -10,7 +10,7 @@ tmp_x = 1:tam_arr(3);
 
  for i=1:tam_arr(1)
      for j=1:tam_arr(2)
-         [ma0(i,j),ma1(i,j),yaj] = m_reglin(tmp_x,M(i,:,j));
+         [ma0(i,j),ma1(i,j),yaj] = f_reglin(tmp_x,M(i,:,j));
      end
  end
 end
