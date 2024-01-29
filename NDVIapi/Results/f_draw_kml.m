c35b577c7e0e08b3f6a53afaadfbd4f1d77de718
rogeliogarciardz <rogeliogarciardz@gmail.com>
2024-01-29 12:09:10 -0600

    Fix function issuses

--- NDVIapi/Results/f_draw_kml.m
+++ NDVIapi/Results/f_draw_kml.m
@@ -1,21 +1,42 @@
-function f_draw_kml(dir_data,kml,plinewi,pcolor,varargin)
+function f_draw_kml(dir_data,kml,varargin)
+    % f_draw_kml draw a kml file over a map. 
+    %
+    %
+    % f_draw_kml("C:\dirdata","kmlfile") draw a kml with a thin black line 
+    %
+    % f_draw_kml("C:\dirdata","kmlfile",n) draw a kml whit a npx black line 
+    %
+    % f_draw_kml("C:\dirdata","kmlfile",n,color)  draw a kml whit a npx color line 
+    %
+    % f_draw_kml("C:\dirdata","kmlfile",n,color,"Label") draw a kml whit a
+    % npx color line and over a label 
+    %
+    %  Written by:  (2024) Rogelio García Rodríguez 
+    if(nargin<2)
+        error('Faltan parametros a la función');
+    elseif(nargin==2)
+        plinewi=1;
+        pcolor='k';
+    elseif(nargin==3 && isnumeric(varargin{1}))
+        plinewi = varargin{1};
+        pcolor='k';
+    elseif(nargin==4 && isnumeric(varargin{1}))
+        plinewi = varargin{1};
+        pcolor=varargin{2};
+    elseif(nargin==5 && isnumeric(varargin{1}) && isstring(varargin{3}))
+        plinewi = varargin{1};
+        pcolor=varargin{2};
+    else
+        error('Parametros incorrectos');
+    end
 
     [bndry_lon,bndry_lat,~] = read_kml(dir_data+'KML\'+kml+'.kml');
     m_line(bndry_lon,bndry_lat,'linewi',plinewi,'color',pcolor); 
-   %m_hatch(bndry_lon,bndry_lat,'speckle',7,1,'color','g'); % ...with hatching added.
+    %disp("PL "+plinewi)
+    %disp("PC "+pcolor)
 
-    nVarargs = length(varargin);
-    
-    %fprintf('Inputs in varargin(%d):\n',nVarargs);
-
-    if nVarargs==1
-
-        m_text( mean(bndry_lon,'all'), mean(bndry_lat,'all'),sprintf('%s',varargin{1}));
-        %disp(varargin(1))
+    if nargin==5
+        m_text( mean(bndry_lon,'all'), mean(bndry_lat,'all'),sprintf('%s',varargin{3}));
+        %disp(varargin(3))
     end
-%     for k = 1:nVarargs 
-%         fprintf(' %d\n', varargin{k})
-%     end
-
-
 end
